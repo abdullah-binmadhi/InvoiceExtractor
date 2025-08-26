3950bdac850c55a0803c46ea1649ec00a9a9c376
abdullah-binmadhi <89339365+abdullah-binmadhi@users.noreply.github.com>
2025-08-26 22:38:23 +0800

    Remove Render deployment files and update README to focus on GitHub Pages version

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install system dependencies
-apt-get update
-apt-get install -y tesseract-ocr poppler-utils
-
-# Install Python dependencies (this will be done automatically by Render)
-pip install -r backend/requirements.txt
